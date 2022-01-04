4714548fe3fae33ec1fc1572625b2e55c6fdf653
abu-bakarr <abangura14@yahoo.com>
2022-01-04 10:53:01 +0000

    Added excersie 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-# Creates a file owened by www-data
-file { '/tmp/holberton':
-    ensure  => file,
-    path    => '/tmp/holberton',
-    mode    => '0744',
-    owner   => 'www-data',
-    group   => 'www-data',
-    content => 'I love Puppet',
+# My first puppet file
+file { 'holberton':
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
