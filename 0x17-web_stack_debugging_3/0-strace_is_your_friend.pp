2b5e4d3db71ddb3c808c35ed1843c21a9463fcb7
abu-bakarr <abangura14@yahoo.com>
2022-02-16 16:50:22 +0000

    Add file 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
-#  Strace is your friend
-exec { 'change-now':
-  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
-  path    => '/bin/'
-}
+# .php
+exec { 'setting.php':
+  path    => ['/bin'],
+  command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
+  }
